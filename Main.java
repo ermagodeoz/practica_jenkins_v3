991182810d1178a5fd6c924dd604804b56ae2fb0
Antonio Santos <antoniosantosramos@gmail.com>
2020-07-31 04:30:44 +0200

    commit 01

---
+++ Main.java
@@ -0,0 +1,13 @@
+javac Main.javac
+/**
+ * @author Antonio Santos Ramos
+ * 
+ **/
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("hola a todos y todas en developmeent");
+	}
+
+}
